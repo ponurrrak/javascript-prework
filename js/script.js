782dbdacb1cf1f265998a9aa62a22a66b41ba5ce
Maciek Mielczarek <maciejka-67@o2.pl>
2021-08-07 18:54:31 +0200

    Add logic of player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove;
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == 2) {
+  playerMove = 'papier';
+} else {
+  playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
