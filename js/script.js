effe7fbc801d99b537d10c96ca4221ed860a5ad3
Maciek Mielczarek <maciejka-67@o2.pl>
2021-08-07 20:33:33 +0200

    Add logic responsible for decision who wins

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove;
+let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if (playerInput == '1') {
   playerMove = 'kamień';
-} else if (playerInput == 2) {
+} else if (playerInput == '2') {
   playerMove = 'papier';
-} else {
+} else if (playerInput == '3') {
   playerMove = 'nożyce';
 }
 
@@ -20,7 +20,7 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove;
 
-if(randomNumber == 1){
+if (randomNumber == 1) {
   computerMove = 'kamień';
 } else if (randomNumber == 2) {
   computerMove = 'papier';
@@ -29,3 +29,19 @@ if(randomNumber == 1){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let whoWins = 'Niestety, przegrałeś.';
+
+let playerWins = (playerMove == 'papier' && computerMove == 'kamień');
+playerWins = playerWins || (playerMove == 'kamień' && computerMove == 'nożyce');
+playerWins = playerWins || (playerMove == 'nożyce' && computerMove == 'papier');
+
+if (playerMove == 'nieznany ruch') {
+  whoWins = 'Podałeś wartość inną niż 1, 2 lub 3. Dlatego gra nie może zostać rozstrzygnięta. Spróbuj jeszcze raz.';
+} else if (computerMove == playerMove) {
+  whoWins = 'Mamy remis.';
+} else if (playerWins) {
+  whoWins = 'Brawo. Wygrałeś!';
+}
+
+printMessage(whoWins);
